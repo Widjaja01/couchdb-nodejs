95c2895b8dc583b40cd46cff32cb8b28fe5cd848
Sugi <sw@Sugis-MacBook-Pro.local>
2016-09-15 20:30:27 -0700

    update tab width from 4 to 2

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ var router = express.Router();
 
 // couchdb server healtcheck
 router.get('/healthcheck', function (req, res) {
-	request.get('http://127.0.0.1:5985', function (error, response, body) {
-		if (error) {
-			res.status(500).send(error.message);
-		} else {
-			var statusCode = response.statusCode;
-			res.json(body);
-		}
-	});
+  request.get('http://127.0.0.1:5985', function (error, response, body) {
+    if (error) {
+      res.status(500).send(error.message);
+    } else {
+      var statusCode = response.statusCode;
+      res.json(body);
+    }
+  });
 });
 
 module.exports = router;
