5922a09b76767b69a3cd32e3a3fd60f88f33bf62
Sugi <sw@Sugis-MacBook-Pro.local>
2016-09-16 21:27:19 -0700

    Add a simple API to get the list of all games

--- configs/dbconf.js
+++ configs/dbconf.js
@@ -1,4 +1,7 @@
 module.exports = {
   'dbHost': 'http://127.0.0.1',
-  'dbPort': 5984
+  'dbPort': 5984,
+  'dbName': 'game',
+  'designDocumentName': 'game',
+  'viewName': 'simpleGet'
 }
--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,53 @@ router.get('/healthcheck', function (req, res) {
   });
 });
 
+/**
+ * This is sample API to get list of games
+ *
+ * The Map function
+ * function(doc) {
+ *   emit(null, {'title': doc.title, 'publisher': doc.publisher});
+ * }
+ *
+ */
+
+router.get('/games', function (req, res) {
+  
+  // API http://docs.couchdb.org/en/stable/api/ddoc/views.html#
+  var apiUrl = [dbUri, '/', dbConfig.dbName, '/_design/', 
+    dbConfig.designDocumentName, '/_view/', dbConfig.viewName].join('');
+
+  request.get(apiUrl, function (error, response, body) {
+    if (error) {
+        res.status(500).send(error.message);
+    } else {
+      var statusCode = response.statusCode;
+      var resultJSON = null;
+
+      try {
+        resultJSON = JSON.parse(body);
+      } catch (ex) {
+        resultJSON = null;
+      }
+
+      if (resultJSON === null) {
+        res.status(500).send({'error': 'Error parsing the JSON string from couchDB'});
+      } else if (statusCode !== 200 && resultJSON.error) {
+        res.status(response.statusCode).send(resultJSON);
+      } else {
+        var allGames = [];
+
+        resultJSON.rows.forEach(function (row) {
+          var gameId = row.id;
+          var gameTitle = (row.value && row.value.title) || '';
+          var gamePublisher = (row.value && row.value.publisher) || '';
+          allGames.push( {'id': gameId, 'title': gameTitle, 'publisher': gamePublisher} );
+        });
+        
+        res.json(allGames);
+      }
+    }
+  });
+});
+
 module.exports = router;
